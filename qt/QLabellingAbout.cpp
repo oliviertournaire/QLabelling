ee9c918aaaafd0ad21f5213ac14a71ebd462802d
Olivier Tournaire <olivier.tournaire@gmail.com>
2013-05-12 15:18:33 +0200

    Minimal about dialog

--- qt/QLabellingAbout.cpp
+++ qt/QLabellingAbout.cpp
@@ -1,11 +1,14 @@
 #include "QLabellingAbout.hpp"
 #include "ui_QLabellingAbout.h"
+#include "config.hpp"
 
 QLabellingAbout::QLabellingAbout(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::QLabellingAbout)
 {
     ui->setupUi(this);
+
+    ui->_labelVersion->setText( ui->_labelVersion->text() + QLABELLING_VERSION_STRING );
 }
 
 QLabellingAbout::~QLabellingAbout()
