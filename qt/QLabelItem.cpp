228e87149e1e524b550068f848947b26cd1965aa
Olivier Tournaire <olivier.tournaire@gmail.com>
2013-05-15 00:11:27 +0200

    Issue #2 Add QLabelItem

---
+++ qt/QLabelItem.cpp
@@ -0,0 +1,105 @@
+#include "QLabelItem.hpp"
+
+#include <QDebug>
+#include <QColorDialog>
+#include <QRadioButton>
+#include <QToolButton>
+
+#include "config.hpp"
+
+QLabelItem::QLabelItem(QString labelName, QColor labelColor, QWidget *parent) :
+    QWidget(parent),
+    _labelName(labelName),
+    _radioButtonlabel(new QRadioButton(parent)),
+    _toolButtonLabelColor(new QToolButton(parent)),
+    _labelColor(labelColor)
+{
+    _radioButtonlabel->setText(_labelName);
+    _toolButtonLabelColor->setIconSize( QSize(QLABELLING_TOOLBUTTON_ICON_SIZE,QLABELLING_TOOLBUTTON_ICON_SIZE) );
+    _toolButtonLabelColor->setStyleSheet( QString(QLABELLING_TOOLBUTTON_STYLESHEET_STRING).arg(_labelColor.name()) );
+
+    connectAll();
+}
+
+QLabelItem::~QLabelItem() { disconnectAll(); }
+
+void QLabelItem::connectAll()
+{
+    connect(_radioButtonlabel,
+            SIGNAL(toggled(bool)),
+            this,
+            SLOT(radioButtonLabel_toggled(bool)));
+
+    connect(_toolButtonLabelColor,
+            SIGNAL(clicked(bool)),
+            this,
+            SLOT(toolButtonLabelColor_clicked(bool)));
+}
+
+void QLabelItem::disconnectAll()
+{
+    disconnect(_radioButtonlabel,
+               SIGNAL(toggled(bool)),
+               this,
+               SLOT(radioButtonLabel_toggled(bool)));
+
+    disconnect(_toolButtonLabelColor,
+               SIGNAL(clicked(bool)),
+               this,
+               SLOT(toolButtonLabelColor_clicked(bool)));
+}
+
+void QLabelItem::radioButtonLabel_toggled(bool checked)
+{
+    qDebug() << __FUNCTION__;
+}
+
+void QLabelItem::toolButtonLabelColor_clicked(bool checked)
+{
+    QColor choosedColor = QColorDialog::getColor(_labelColor);
+    if (choosedColor.isValid())
+    {
+        _labelColor = choosedColor;
+        _toolButtonLabelColor->setStyleSheet( QString(QLABELLING_TOOLBUTTON_STYLESHEET_STRING).arg(_labelColor.name()) );
+    }
+}
+
+QToolButton *QLabelItem::toolButtonLabelColor() const
+{
+    return _toolButtonLabelColor;
+}
+
+void QLabelItem::setToolButtonLabelColor(QToolButton *toolButtonLabelColor)
+{
+    _toolButtonLabelColor = toolButtonLabelColor;
+}
+
+QRadioButton *QLabelItem::radioButtonlabel() const
+{
+    return _radioButtonlabel;
+}
+
+void QLabelItem::setRadioButtonlabel(QRadioButton *radioButtonlabel)
+{
+    _radioButtonlabel = radioButtonlabel;
+}
+
+QString QLabelItem::labelName() const
+{
+    return _labelName;
+}
+
+void QLabelItem::setLabelName(const QString &labelName)
+{
+    _labelName = labelName;
+}
+
+QColor QLabelItem::labelColor() const
+{
+    return _labelColor;
+}
+
+void QLabelItem::setLabelColor(const QColor &labelColor)
+{
+    _labelColor = labelColor;
+}
---
+++ qt/QLabelItem.hpp
@@ -0,0 +1,52 @@
+#ifndef QLABELITEM_HPP
+#define QLABELITEM_HPP
+
+#include <QWidget>
+
+class QRadioButton;
+class QToolButton;
+
+/*! \brief Brief Simple container for a generic label.
+ *
+ *  This class is a simple container for a generic label item. It stores various
+ *  information (name, color ...) and QWidgets to enable querying and GUI creation.
+ *  It also handles GUI events.
+ *
+ *  \author Olivier Tournaire
+ */
+class QLabelItem : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit QLabelItem(QString labelName, QColor labelColor = Qt::gray, QWidget *parent = 0);
+    ~QLabelItem();
+    
+    QString labelName() const;
+    void setLabelName(const QString &labelName);
+
+    QRadioButton *radioButtonlabel() const;
+    void setRadioButtonlabel(QRadioButton *radioButtonlabel);
+
+    QToolButton *toolButtonLabelColor() const;
+    void setToolButtonLabelColor(QToolButton *toolButtonLabelColor);
+
+    QColor labelColor() const;
+    void setLabelColor(const QColor &labelColor);
+
+signals:
+    
+public slots:
+    void radioButtonLabel_toggled(bool checked);
+    void toolButtonLabelColor_clicked(bool checked = false);
+
+protected:
+    void connectAll();
+    void disconnectAll();
+
+    QString _labelName;
+    QRadioButton *_radioButtonlabel;
+    QToolButton *_toolButtonLabelColor;
+    QColor _labelColor;
+};
+
+#endif // QLABELITEM_HPP
