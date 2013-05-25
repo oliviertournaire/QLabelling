0f9efd8a78e41afc74872496734c1f26597eef77
Olivier Tournaire <olivier.tournaire@gmail.com>
2013-05-25 23:37:35 +0200

    Issue #5 Add save log method immplementation

--- qt/QLabellingLogWidget.cpp
+++ qt/QLabellingLogWidget.cpp
@@ -2,6 +2,13 @@
 #include "ui_QLabellingLogWidget.h"
 
 #include <QTime>
+#include <QFile>
+#include <QSettings>
+#include <QFileInfo>
+#include <QFileDialog>
+#include <QTextStream>
+
+#include "config.hpp"
 
 QLabellingLogWidget::QLabellingLogWidget(QWidget *parent) :
     QWidget(parent),
@@ -22,8 +29,23 @@ void QLabellingLogWidget::on__toolButtonClear_clicked(bool checked)
 
 void QLabellingLogWidget::on__toolButtonSave_clicked(bool checked)
 {
-    // TODO
-    ;
+    QSettings settings(QLABELLING_ORGANIZATION_STRING, QLABELLING_NAME_STRING);
+    settings.beginGroup("QLabellingMainWindow");
+    QString defaultLogDirectory = settings.value("defaultLogDirectory", "").toString();
+
+    QString fileName = QFileDialog::getSaveFileName(this, tr("Open image to label"), defaultLogDirectory, tr("Text files (*.txt *.log)"));
+    if(!fileName.isNull())
+    {
+        QFile outfile;
+        outfile.setFileName(fileName);
+        outfile.open(QIODevice::Append | QIODevice::Text);
+        QTextStream out(&outfile);
+        out << ui->_textEdit->toPlainText();
+
+        QFileInfo info(fileName);
+        settings.setValue("defaultLogDirectory", info.absolutePath());
+        settings.endGroup();
+    }
 }
 
 void QLabellingLogWidget::logFatalError(const QString& text)
