0c613084284aff35b89e0a21f551ad80c93f8cbc
Olivier Tournaire <olivier.tournaire@gmail.com>
2014-05-23 11:32:25 +0200

    Fix compilation errors (although the code is useless ...)

--- src/QLabelling/findnearestedge.h
+++ src/QLabelling/findnearestedge.h
@@ -1,26 +1,19 @@
 #ifndef FINDNEARESTEDGE_H
 #define FINDNEARESTEDGE_H
-#include "Utils.h"
-#include "utilities/Singleton.hpp"
-typedef CGAL::Arr_segment_traits_2<Kernel>                          Seg_traits;
 
-// Polyline
-typedef CGAL::Arr_polyline_traits_2<Seg_traits>                     Pol_traits;
-typedef Pol_traits::Curve_2                                         Arr_pol_2;
-typedef Pol_traits::X_monotone_curve_2                              Arr_xpol_2;
-typedef Pol_traits::Point_2                                         Arr_pol_point_2;
-typedef CGAL::Arr_face_extended_dcel<Pol_traits, Face_with_color>   Pol_dcel;
-typedef CGAL::Arrangement_with_history_2<Pol_traits, Pol_dcel>      Pol_arr;
-typedef CGAL::Arr_polyline_traits_2< Seg_traits > Traits;
-typedef typename Traits::Curve_2 Curve_2;
-typedef typename Seg_traits::Kernel Kernel;
-typedef typename Kernel::Point_2 Point_2;
-typedef typename Kernel::Segment_2 Segment_2;
-class findnearestedge : public Singleton<findnearestedge>
+#include <CGAL/Qt/Converter.h>
+
+class QGraphicsSceneMouseEvent;
+class QGraphicsScene;
+
+#include "ArrangementTypes.h"
+
+class findnearestedge
 {
 public:
-   findnearestedge();
+   findnearestedge() {}
    Pol_arr::Halfedge_const_handle getnearestedge(QGraphicsSceneMouseEvent* event, Pol_arr *arr, QGraphicsScene* _scene);
+
 private:
    CGAL::Qt::Converter< Kernel > _converter;
 };
